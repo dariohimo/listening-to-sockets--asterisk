cdaf085bee992b93919091ef96f4cd86347d36e3
Alan Cornejo <alancornejo@icloud.com>
2017-05-10 18:43:56 -0500

    - Modificacion de los eventos para devolver parametros a diferentes panel del dashboard    realizando update, remove, addgin a calls waiting, calls inbound, calls outbound.
     - Refactorizacion del codigo para reusar funciones de envios de socket

--- index.js
+++ index.js
@@ -50,10 +50,8 @@ io.adapter(socketredis({ host: redisHost, port: redisPort, password: redisPasswo
 var iosocket = io.sockets.on('connection', (socket) => {
   console.log('Connection to client established ' + socket.id)
 
-  socket.on('listAgentConnect', data => {
-    agents.shows().then(data => {
-      data.forEach(agent => socket.emit('QueueMemberAdded', { QueueMemberAdded: agent }))
-    })
+  socket.on('listOther', data => {
+    agents.shows().then(data => data.forEach(agent => socket.emit('AddOther', agent)))
     .catch(err => console.log('Error al obtener agentes :' + err))
   })
 
@@ -67,7 +65,7 @@ var iosocket = io.sockets.on('connection', (socket) => {
   })
 
   socket.on('createAgent', data => {
-    agents.createAgent(data).then(data => iosocket.emit('QueueMemberAdded', { QueueMemberAdded: data }))
+    agents.memberCreate(data).then(data => { iosocket.emit('AddOther', data) })
     .catch(err => { console.log('Error adding agent on the dashboard :' + err) })
   })
 })
@@ -81,7 +79,7 @@ ami.on('error', err => {
 * [Capturar el evento Queue Member Added que se produce en el servicio de asterisk]
 */
 ami.on('eventQueueMemberAdded', data => {
-  agents.add(data).then(data => iosocket.emit('QueueMemberAdded', { QueueMemberAdded: data }))
+  agents.memberAdd(data).then(data => iosocket.emit('UpdateOther', data))
   .catch(err => console.log('Error adding agent on the dashboard :' + err))
 })
 
@@ -89,7 +87,7 @@ ami.on('eventQueueMemberAdded', data => {
 * [Capturar el evento Queue Member Removed que se produce en el servicio de asterisk]
 */
 ami.on('eventQueueMemberRemoved', data => {
-  agents.del(data).then(data => iosocket.emit('QueueMemberRemoved', { NumberAnnexed: data }))
+  agents.memberRemoved(data).then(data => iosocket.emit('RemoveOther', data))
   .catch(err => { console.log('Error removing agent on the dashboard :' + err) })
 })
 
@@ -97,7 +95,7 @@ ami.on('eventQueueMemberRemoved', data => {
 * [Capturar el evento Queue Member Pause que se produce en el servicio de asterisk]
 */
 ami.on('eventQueueMemberPause', data => {
-  agents.pause(data).then(data => iosocket.emit('QueueMemberChange', { QueueMemberChange: data }))
+  agents.memberPause(data).then(data => iosocket.emit('UpdateOther', data))
   .catch(err => { console.log('Error pausing agent on the dashboard :' + err) })
 })
 
@@ -105,7 +103,7 @@ ami.on('eventQueueMemberPause', data => {
 * [Capturar el evento de timbrado de la llamada entrante que se produce en el servicio de asterisk]
 */
 ami.on('eventNewConnectedLine', data => {
-  agents.ringInbound(data).then(data => sendSockets(data, false))
+  agents.ringInbound(data).then(data => pruebas(data,'eventNewConnectedLine'))
   .catch(err => { console.log('Error al mostrar ring de entrante :' + err) })
 })
 
@@ -113,7 +111,7 @@ ami.on('eventNewConnectedLine', data => {
 * [Capturar el evento Answer de la llamada entrante que se produce en el servicio de asterisk]
 */
 ami.on('eventAgentConnect', data => {
-  agents.answerInbound(data).then(data => sendSockets(data, true))
+  agents.answerInbound(data).then(data => pruebas(data,'eventAgentConnect'))
   .catch(err => { console.log('Error al capturar (answer) de la llamada entrante :' + err) })
 })
 
@@ -121,15 +119,15 @@ ami.on('eventAgentConnect', data => {
 * [Capturar el evento de timbrado de la llamada saliente que se produce en el servicio de asterisk]
 */
 ami.on('eventNewstate', data => {
-  agents.ringOutbound(data).then(data => sendSockets(data, true))
-  .catch(err => { console.log('Error al mostrar ring de salientes :' + err) })
+  agents.ringAnswerOutbound(data).then(data => pruebas(data,'eventNewstate'))
+  .catch(err => { console.log('Error al mostrar Ring o Answer de Llamada Outbound :' + err) })
 })
 
 /**
 * [Capturar el evento de corte de llamada sea entrante y/o saliente que se produce en el servicio de asterisk]
 */
 ami.on('eventHangup', data => {
-  agents.hangup(data).then(data => sendSockets(data, true))
+  agents.hangup(data).then(data => pruebas(data, 'eventHangup'))
   .catch(err => { console.log('Error al cortar (hangup) llamadas salientes y/o entrantes :' + err) })
 })
 
@@ -139,22 +137,22 @@ ami.on('eventHangup', data => {
 ami.on('eventBlindTransfer', data => sendSockets(agents.transferUnattended(data), true))
 
 ami.on('eventHold', data => {
-  agents.hold(data).then(data => sendSockets(data, true))
+  agents.hold(data).then(data => pruebas(data, 'eventHold'))
   .catch(err => { console.log('Error al mostrar Hold :' + err) })
 })
 
 ami.on('eventUnhold', data => {
-  agents.unhold(data).then(data => sendSockets(data, true))
+  agents.unhold(data).then(data => pruebas(data, 'eventUnhold'))
   .catch(err => { console.log('Error al mostrar UnHold :' + err) })
 })
 
 ami.on('eventQueueCallerJoin', data => {
-  callWaiting.create(data).then(data => sendCallWaiting(data, 'AddCallWaiting'))
+  callWaiting.create(data).then(data => iosocket.emit('AddCallWaiting', data))
   .catch(err => { console.log('Error al insertar llamadas encoladas :' + err) })
 })
 
 ami.on('eventQueueCallerLeave', data => {
-  callWaiting.delete(data).then(data => sendCallWaiting(data, 'RemoveCallWaiting'))
+  callWaiting.delete(data).then(data => iosocket.emit('RemoveCallWaiting', data))
   .catch(err => { console.log('Error al eliminar llamadas encoladas :' + err) })
 })
 
@@ -162,32 +160,90 @@ ami.on('eventAttendedTransfer', data => {
   // console.log(data)
 })
 
-function sendCallWaiting (data, nameEventEmit) {
-  if (data) {
-    console.log(data)
-    iosocket.emit(nameEventEmit, { CallWaiting: data })
+const pruebas = (dataAgent, event = false) => {
+  let preEventID = dataAgent.event_id_old
+  let postEventID = dataAgent.event_id
+  
+  // Controlando el Hangup 
+  if (event === 'eventHangup') {
+    console.log(dataAgent)
+    console.log(`eventHangup ----  Post : ${postEventID}  - Pre : ${preEventID}`)
+    sendSocketInbound(dataAgent, true, false, false)
+    sendSocketOutbound(dataAgent, true, false, false)
+    sendSocketOther(dataAgent, false, false, true)
+    sendSocketsExtras(dataAgent, true, true)
+  }
+
+  // Controlando llamadas Outbound, Ring Outbound, Hold Outbound
+  if ((event === 'eventNewstate' || event === 'eventHold' || event === 'eventUnhold') && (postEventID === '9' || postEventID === '13' || postEventID === '17')) {
+    console.log(`eventNewstate ----  Post : ${postEventID}  - Pre : ${preEventID}`)
+    if (preEventID === '1') {
+      sendSocketOther(dataAgent, true, false, false)
+      sendSocketOutbound(dataAgent, false, false, true)
+    }
+    if (preEventID === '9' || preEventID === '13' || preEventID === '17') sendSocketOutbound(dataAgent, false, true, false)
+    if (postEventID === '9' || postEventID === '13' ) sendSocketsExtras(dataAgent, true, true) 
+    else sendSocketsExtras(dataAgent, false, true) 
+  }
+
+  // Controlando llamadas Ring Inbound y Inbound
+  if ((event === 'eventNewConnectedLine' || event === 'eventAgentConnect' || event === 'eventHold' || event === 'eventUnhold') && (postEventID === '8' || postEventID === '12' || postEventID === '16')) {
+    console.log(`eventNewConnectedLine ----  Post : ${postEventID}  - Pre : ${preEventID}`)
+    if (preEventID === '1') {
+      sendSocketOther(dataAgent, true, false, false)
+      sendSocketInbound(dataAgent, false, false, true)
+    }
+    if (preEventID === '8' || preEventID === '12' || preEventID === '16') sendSocketInbound(dataAgent, false, true, false)
+    if (postEventID === '8' || postEventID === '12') sendSocketsExtras(dataAgent, true, true) 
+    else sendSocketsExtras(dataAgent, false, true) 
   }
 }
 
-function sendSockets (data, isPause) {
-  if (data) {
-    // console.log(isPause)
-    // console.log(data)
-    if (isPause) actionAmi.pauseQueue(data)
-    else eventDetail.insertEvent(data)
-
-    iosocket.emit('QueueMemberChange', { QueueMemberChange: data })
-    iosocket.in('panelAgent:' + data.number_annexed).emit('statusAgent', {
-      NameEvent: data.name_event,
-      EventId: data.event_id
+const sendSocketsExtras = (dataAgent, isPause, isDetailEvents) => {
+  if (dataAgent) {
+    if (isPause) actionAmi.pauseQueue(dataAgent)
+    if (isDetailEvents) eventDetail.insertEvent(dataAgent)
+    iosocket.in('panelAgent:' + dataAgent.number_annexed).emit('statusAgent', {
+      NameEvent: dataAgent.name_event,
+      EventId: dataAgent.event_id
     })
   }
+}
+
+const sendSocketOther = (dataAgent, isRemoveOther, isUpdateOther, isAddOther) => {
+  if (isRemoveOther) iosocket.emit('RemoveOther', dataAgent)
+  if (isUpdateOther) iosocket.emit('UpdateOther', dataAgent)
+  if (isAddOther) iosocket.emit('AddOther', dataAgent)
+}
+
+const sendSocketOutbound = (dataAgent, isRemoveOutbound, isUpdateOutbound, isAddOutbound) => {
+  if (isRemoveOutbound) iosocket.emit('RemoveOutbound', dataAgent)
+  if (isUpdateOutbound) iosocket.emit('UpdateOutbound', dataAgent)
+  if (isAddOutbound) iosocket.emit('AddOutbound', dataAgent)
+}
+
+const sendSocketInbound = (dataAgent, isRemoveInbound, isUpdateInbound, isAddInbound) => {
+  if (isRemoveInbound) iosocket.emit('RemoveInbound', dataAgent)
+  if (isUpdateInbound) iosocket.emit('UpdateInbound', dataAgent)
+  if (isAddInbound) iosocket.emit('AddInbound', dataAgent)
 }
 
 ami.on('eventAny', data => {
   // console.log(data)
 })
 
+/*
+const sendSockets = (dataAgent, isPause, isDetailEvents) => {
+  if (dataAgent) {
+    if (isPause) actionAmi.pauseQueue(dataAgent)
+    if (isDetailEvents) eventDetail.insertEvent(dataAgent)
+
+    iosocket.emit('UpdateOther', dataAgent)
+    updateStatusEventFront(dataAgent)
+  }
+}
+
+ */
 // QueueCallerJoin : cuando un cliente se agrega a la cola
 // QueueCallerLeave : cuando un cliente deja la cola o es contestada
 // QueueMemberStatus : ver el estado del agente en la cola
